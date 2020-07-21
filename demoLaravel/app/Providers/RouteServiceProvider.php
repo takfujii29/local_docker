6a376d71d933c53d656fcc5e1f773f63bbe8ee7a
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-07-21 11:41:20 +0900

    routesの変更
    api -> backendに変更

--- demoLaravel/app/Providers/RouteServiceProvider.php
+++ demoLaravel/app/Providers/RouteServiceProvider.php
@@ -46,6 +46,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapBackendRoutes();
+
         //
     }
 
@@ -76,5 +78,13 @@ class RouteServiceProvider extends ServiceProvider
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
+    }
+
+    protected function mapBackendRoutes()
+    {
+        Route::prefix('backend')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/backend.php'));
     }
 }
